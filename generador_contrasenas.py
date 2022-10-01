addc760c566e2a086de374f6ce6646ec6f84a0a3
CrisChG <92888657+CrisChG@users.noreply.github.com>
2022-09-30 23:19:56 -0500

    Update generador_contrasenas.py

--- generador_contrasenas.py
+++ generador_contrasenas.py
@@ -5,19 +5,12 @@ MAYUSCULAS=MINUSCULAS.upper()
 NUMEROS='1234567890'
 CARACTERES_ESPECIALES='!@#$%^&*()_-{[}]/<>'
 
-tamano=int(input('¿Cuantos caracteres quieres que tenga tu contraseña? '))
-pedir_ma= input('¿Quieres que tu contraseña contenga mayusculas? ')
-pedir_ce= input('¿Quieres que tu contraseña contenga caracteres especiales? ')
-rango=int(input('Cuantas contraseñas deseas generar '))
 generar= None
 password=None
 
 conjunto=MINUSCULAS+NUMEROS
 
-if pedir_ma=='si' or pedir_ma=='Si':
-    conjunto=conjunto+MAYUSCULAS
-if pedir_ce=='si' or pedir_ce=='Si':
-    conjunto=conjunto+CARACTERES_ESPECIALES
+lista=[]
 
 def generarPasswords(rango,generar,conjunto,tamano,password):
     for _ in range(rango):
@@ -25,4 +18,28 @@ def generarPasswords(rango,generar,conjunto,tamano,password):
         password = ''.join(generar)
         print (password)
 
-generarPasswords(rango,generar,conjunto,tamano,password)
+opcion=input('Si deseas generar una o varias contraseñas selecciona 1 \n'
+            'Si deseas guardar tu contraseña generada selecciona 2\n'
+            'Si deseas recuperar las contrañas guardadas selecciona 3\n:')
+while opcion!='0' or opcion<'4' or opcion == 'no':
+    if opcion=='1':
+        tamano=int(input('¿Cuantos caracteres quieres que tenga tu contraseña?: '))
+        pedir_ma= input('¿Quieres que tu contraseña contenga mayusculas?: ')
+        if pedir_ma=='si' or pedir_ma=='Si':
+             conjunto=conjunto+MAYUSCULAS
+        pedir_ce= input('¿Quieres que tu contraseña contenga caracteres especiales?: ')
+        if pedir_ce=='si' or pedir_ce=='Si':
+             conjunto=conjunto+CARACTERES_ESPECIALES
+        rango=int(input('Cuantas contraseñas deseas generar? :'))
+        generarPasswords(rango,generar,conjunto,tamano,password)
+        opcion=input('¿Deseas elegir otra opcion?: ')
+    elif opcion=='2':
+        lista.append(input('¿Introduce la nueva contraseña a guardar?: '))
+        opcion=input('¿Deseas elegir otra opcion?: ')
+    elif opcion=='3':
+        print(lista)
+        opcion=input('¿Deseas elegir otra opcion?: ')
+    elif opcion=='no' or 'No':  
+        break
+else:
+    print('Opcion invalida')
